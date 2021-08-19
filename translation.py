3bfa4ce9ef46bdc9b4fd6462ca4dbfb3ee9787c9
Akila-Widuruwan <89211127+Akila-Widuruwan@users.noreply.github.com>
2021-08-19 21:46:18 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -35,7 +35,7 @@ URL | filename | username | password"""
 Telegram ID: <code>{}</code>
 Plan name: Free Cloned User
 Expires on: 31/12/2020"""
-    HELP_USER = """🙋 Hai am URL Uploader bot..
+    HELP_USER = """🙋 Hai I am URL Uploader bot..
     
 1. Send url 🔗(Link|New Name with Extension).
 2. Send Custom Thumbnail 🖼(Optional).
